b899a3fdbd0c62097d7e15939822136c3bfa36c9
zhaoleisheng <304543872@163.com>
2017-08-01 11:51:04 +0900

    add to username

--- src/helloWord.java
+++ src/helloWord.java
@@ -3,7 +3,9 @@ public class helloWord {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World!");
+		String userName = "zxxx";
+		String welCome = "Welcome to GitHub";
+		System.out.println("Hi"+userName+"Hello World!"+welCome);
 	}
 
 }
